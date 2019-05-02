bb1dc57fac2b515ae98ab9b811a88843d1c9d4dc
Son Chau <soncqb@gmail.com>
2019-05-02 13:50:07 -0500

    add card

--- lib/todo_card.dart
+++ lib/todo_card.dart
@@ -1,6 +1,44 @@
 import 'package:flutter/material.dart';
 
 class TodoCard extends StatelessWidget {
+  final taskCountSection = Container(
+    padding: EdgeInsets.only(left: 8,),
+    child: Text('9 Tasks', style: TextStyle(color: Colors.grey[500]),),
+  );
+  final taskTypeSection = Container(
+    padding: EdgeInsets.only(left:8),
+    child: Text('Personal', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.grey),),
+  );
+  final headerSection = Expanded(
+    child : Container(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Icon(Icons.account_circle),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child:  Icon(Icons.more_vert),
+          ),
+
+        ],
+      ),
+    ),
+  );
+
+  final bottomSection = Container(
+    padding: EdgeInsets.all(8),
+    child: Row(
+      children: <Widget>[
+        Text('83%', style: TextStyle(color: Colors.grey[500]),),
+      ],
+    ),
+  );
+
 
 
   @override
@@ -13,11 +51,12 @@ class TodoCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('Row with Icon and expand dot'),
-          Text('9 Tasks'),
-          Text('Personal'),
-          Text('Progress bar'),
+          headerSection,
+          taskCountSection,
+          taskTypeSection,
+          bottomSection,
         ],
       ),
     );
